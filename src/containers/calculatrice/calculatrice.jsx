8c56f7604b040c00d276ad2abcaef9a8ce54b739
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-14 11:06:34 +0100

    Accessibility id

--- src/containers/calculatrice/calculatrice.jsx
+++ src/containers/calculatrice/calculatrice.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 
 const calcOperation = new Map([
     ['add', { display: '+' }],
@@ -11,6 +11,7 @@ console.log(calcOperation);
 
 export default function Calculatrice() {
 
+    const inputId = useId();
     const [nb1, setNb1] = useState('');
     const [nb2, setNb2] = useState('');
     const [op, setOp] = useState(calcOperation.keys[0]);
@@ -21,14 +22,14 @@ export default function Calculatrice() {
             <h2>Calculatrice</h2>
             <form>
                 <div>
-                    <label htmlFor="">Nb1 : </label>
-                    <input type="text"
+                    <label htmlFor={inputId+'-nb1'}>Nb1 : </label>
+                    <input type="text" id={inputId+'-nb1'} required
                         value={nb1}
                         onChange={(e) => setNb1(e.target.value)} />
                 </div>
                 <div>
-                    <label htmlFor="">Opé : </label>
-                    <select required
+                    <label htmlFor={inputId+'-op'}>Opé : </label>
+                    <select id={inputId+'-op'} required
                         value={op}
                         onChange={(e) => setOp(e.target.value)}>
                         {/* <option value='' disabled>Veuillez selection une opération</option> */}
@@ -38,8 +39,8 @@ export default function Calculatrice() {
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="">Nb2 : </label>
-                    <input type="text"
+                    <label htmlFor={inputId+'-nb2'}>Nb2 : </label>
+                    <input type="text" id={inputId+'-nb2'} required
                         value={nb2}
                         onChange={(e) => setNb2(e.target.value)} />
                 </div>
@@ -47,8 +48,8 @@ export default function Calculatrice() {
                     <button>Calculer</button>
                 </div>
                 <div>
-                    <label htmlFor="">Res : </label>
-                    <input type="text"
+                    <label htmlFor={inputId+'-res'}>Res : </label>
+                    <input type="text" id={inputId+'-res'}
                         value={res}
                         readOnly />
                 </div>
